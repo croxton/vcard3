cf81051402abf75c5378646851f5e1c2beeb8eb6
croxton <mcroxton@hallmark-design.co.uk>
2016-05-23 15:52:34 +0100

    First commit

---
+++ addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+    'author'      => 'Max Lazar, updated for EE3 by Mark Croxton',
+    'author_url'  => 'http://wiseupstudio.com/',
+    'name'        => 'vCard3',
+    'description' => 'Create your own vCard.',
+    'version'     => '3.0.0',
+    'namespace'   => 'vCard3'
+);
